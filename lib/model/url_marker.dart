ce12dab0db5da052a00d615b71e037d1ad0edeb5
KimHyungSik <colrog968@gmail.com>
2025-01-12 16:56:14 +0900

    json

--- lib/model/url_marker.dart
+++ lib/model/url_marker.dart
@@ -23,4 +23,34 @@ class UrlBookmark {
     this.isFavorite = false,
   })  : id = id ?? const Uuid().v4(), // UUID 생성
         createdAt = createdAt ?? DateTime.now(); // 생성 날짜 기본값
+
+  // JSON 직렬화
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'url': url,
+      'title': title,
+      'description': description,
+      'createdAt': createdAt.toIso8601String(),
+      'folder': folder,
+      'tags': tags,
+      'iconPath': iconPath,
+      'isFavorite': isFavorite,
+    };
+  }
+
+  // JSON 역직렬화
+  factory UrlBookmark.fromJson(Map<String, dynamic> json) {
+    return UrlBookmark(
+      id: json['id'],
+      url: json['url'],
+      title: json['title'],
+      description: json['description'],
+      createdAt: DateTime.parse(json['createdAt']),
+      folder: json['folder'],
+      tags: (json['tags'] as List<dynamic>?)?.cast<String>(),
+      iconPath: json['iconPath'],
+      isFavorite: json['isFavorite'] ?? false,
+    );
+  }
 }
