aaac2abe96f09c7d7abdf33d52339d301ad828d5
KimHyungSik <colrog968@gmail.com>
2025-01-12 23:41:22 +0900

    manager singleton

--- lib/bookmark_manager.dart
+++ lib/bookmark_manager.dart
@@ -5,12 +5,16 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'model/url_marker.dart';
 
 class UrlBookmarkManager {
-  static const String _storageKey = 'UrlBookmarks'; // 로컬 저장 키
+  UrlBookmarkManager._privateConstructor();
+  static final UrlBookmarkManager _instance = UrlBookmarkManager._privateConstructor();
+  // 4. 외부에서 접근할 수 있는 인스턴스
+  factory UrlBookmarkManager() => _instance;
 
-  List<UrlBookmark> _UrlBookmarks = [];
+  static const String _storageKey = 'UrlBookmarks'; // 로컬 저장 키
+  List<UrlBookmark> _urlBookmarks = [];
 
   // 북마크 리스트 가져오기
-  List<UrlBookmark> get UrlBookmarks => List.unmodifiable(_UrlBookmarks);
+  List<UrlBookmark> get urlBookmarks => List.unmodifiable(_urlBookmarks);
 
   // 북마크 로드 (SharedPreferences에서 가져오기)
   Future<void> loadUrlBookmarks() async {
@@ -19,41 +23,41 @@ class UrlBookmarkManager {
 
     if (storedData != null) {
       final List<dynamic> jsonList = json.decode(storedData);
-      _UrlBookmarks = jsonList.map((item) => UrlBookmark.fromJson(item)).toList();
+      _urlBookmarks = jsonList.map((item) => UrlBookmark.fromJson(item)).toList();
     }
   }
 
   // 북마크 저장 (SharedPreferences에 저장)
   Future<void> saveUrlBookmarks() async {
     final prefs = await SharedPreferences.getInstance();
-    final String jsonData = json.encode(_UrlBookmarks.map((b) => b.toJson()).toList());
+    final String jsonData = json.encode(_urlBookmarks.map((b) => b.toJson()).toList());
     await prefs.setString(_storageKey, jsonData);
   }
 
   // 북마크 추가
-  Future<void> addUrlBookmark(UrlBookmark UrlBookmark) async {
-    _UrlBookmarks.add(UrlBookmark);
+  Future<void> addUrlBookmark(UrlBookmark urlBookmark) async {
+    _urlBookmarks.add(urlBookmark);
     await saveUrlBookmarks();
   }
 
   // 북마크 수정
   Future<void> updateUrlBookmark(String id, UrlBookmark updatedUrlBookmark) async {
-    final int index = _UrlBookmarks.indexWhere((UrlBookmark) => UrlBookmark.id == id);
+    final int index = _urlBookmarks.indexWhere((urlBookMark) => urlBookMark.id == id);
     if (index != -1) {
-      _UrlBookmarks[index] = updatedUrlBookmark;
+      _urlBookmarks[index] = updatedUrlBookmark;
       await saveUrlBookmarks();
     }
   }
 
   // 북마크 삭제
   Future<void> deleteUrlBookmark(String id) async {
-    _UrlBookmarks.removeWhere((UrlBookmark) => UrlBookmark.id == id);
+    _urlBookmarks.removeWhere((urlBookMark) => urlBookMark.id == id);
     await saveUrlBookmarks();
   }
 
   // 북마크 초기화
   Future<void> clearUrlBookmarks() async {
-    _UrlBookmarks.clear();
+    _urlBookmarks.clear();
     await saveUrlBookmarks();
   }
 }
