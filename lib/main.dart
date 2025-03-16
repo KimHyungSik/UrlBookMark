51c1d01812ad34e09b4566c496731b7a45541e79
KimHyungSik <colrog968@gmail.com>
2025-03-16 23:02:53 +0900

    로컬라이즈 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,28 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:url_book_marker/screens/bookmark_list_screen.dart';
 
-void main() {
-  runApp(const ProviderScope(child: MainScreen()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    ProviderScope(
+      child: EasyLocalization(
+        supportedLocales: const [
+          Locale('en', 'US'),
+          Locale('ko', 'KR'),
+        ],
+        path: 'assets/translations',
+        fallbackLocale: const Locale('en', 'US'),
+        child: MaterialApp(
+          home: const MainScreen(),
+          debugShowCheckedModeBanner: false,
+        ),
+      ),
+    ),
+  );
 }
 
 class MainScreen extends StatelessWidget {
@@ -13,6 +32,9 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: const BookmarkListScreen(),
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       debugShowCheckedModeBanner: false,
     );
   }
